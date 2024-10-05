62f3ad95c17a82c27660c22a1e67daf3417d5aff
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2024-10-05 22:11:44 +0900

    Title: 불안한 무빙워크, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241005/불안한 무빙워크/unstable-moving-walk.py
+++ 241005/불안한 무빙워크/unstable-moving-walk.py
@@ -6,6 +6,7 @@ up = deque(l[:n])
 down = deque(l[n:][::-1])
 pos = deque([])
 cnt = 0
+zero_cnt = 0
 
 def rotate(pos,up,down):
     new_pos = deque([i+1 for i in list(pos)]) # 한 칸 이동시키고
@@ -15,25 +16,30 @@ def rotate(pos,up,down):
     down.append(up.pop())
     return new_pos,up,down
 
-def move(pos,up,down): #pos:deque
+def move(pos,up,down,zero_cnt): #pos:deque
     new_pos = deque([])
     while pos:
         cur = pos.pop()
         if cur+1 == n-1: #n에 도달하면 즉시 내림
             up[n-1] -= 1
         elif cur+1 < n-1 and cur+1 not in pos and up[cur+1] != 0:
+            if up[cur+1] == 1:
+                zero_cnt += 1
             new_pos.append(cur+1)
             up[cur+1] -= 1
-    return new_pos,up,down
+            
+    return new_pos,up,down,zero_cnt
 
 def put(pos,up,down):
     pos.appendleft(0)
     return pos,up,down
 
-while up.count(0) + down.count(0) < k:
+while zero_cnt < k:
     pos,up,down = rotate(pos,up,down)
-    pos,up,down = move(pos,up,down)
+    pos,up,down,zero_cnt = move(pos,up,down,zero_cnt)
     if 0 not in pos and up[0] != 0:
+        if up[0] == 1:
+            zero_cnt += 1
         pos.appendleft(0)
         up[0] -= 1
     cnt += 1

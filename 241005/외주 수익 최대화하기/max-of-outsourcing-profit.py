0aa224aab3da1e4e7e2b91142fee7d597a7cf6b5
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2024-10-05 11:35:18 +0900

    Title: 외주 수익 최대화하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241005/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 241005/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,18 +1,20 @@
-import sys
-input = sys.stdin.readline
+def max_profit(n, jobs):
+    jobs.sort(key=lambda x: x[0])
+    dp = [0] * (n + 1)
+    
+    for i in range(1, n + 1):
+        dp[i] = dp[i-1]
+
+        for t, p in jobs:
+            if i == t:
+                dp[i] = max(dp[i], dp[i-t] + p)
+    
+    return dp[n]
 
 n = int(input())
 jobs = []
 for _ in range(n):
-    jobs.append(tuple(map(int,input().split())))
+    t, p = map(int, input().split())
+    jobs.append((t, p))
 
-jobs.sort(lambda x: x[0])
-dp = [0]*(n+1)
-
-for i in range(1,n+1):
-    dp[i] = dp[i-1]
-    for t,p in jobs:
-        if i == t:
-            dp[i] = max(dp[i],dp[i-t]+p)
-
-print(max(dp))
+print(max_profit(n, jobs))

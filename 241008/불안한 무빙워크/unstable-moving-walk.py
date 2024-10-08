7f00087642f892fbf43d2109633e7caf69e99cac
Minhyeok Kim <99781357+kyleidea1@users.noreply.github.com>
2024-10-08 16:54:29 +0900

    Title: 불안한 무빙워크, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241008/불안한 무빙워크/unstable-moving-walk.py
+++ 241008/불안한 무빙워크/unstable-moving-walk.py
@@ -13,16 +13,16 @@ def simulation():
     safety.appendleft(safety.pop())
     for i in range(len(ppl)):
         ppl[i] += 1
-        if ppl[i] == n-1:
+        if ppl[i] == n:
             ppl.pop()
 
     for i in range(len(ppl)):
-        if ppl[i] < n-1 and ppl[i]+1 not in ppl:
+        if ppl[i] < n and ppl[i]+1 not in ppl:
             ppl[i] += 1
             safety[ppl[i]] -= 1
             if safety[ppl[i]] == 0:
                 zero_cnt += 1
-            if ppl[i] == n-1:
+            if ppl[i] == n:
                 ppl.pop()
 
     if 0 not in ppl and safety[0] > 0:
@@ -35,4 +35,4 @@ cnt = 0
 while zero_cnt < k:
     simulation()
     cnt += 1
-print(cnt+1)
+print(cnt)
